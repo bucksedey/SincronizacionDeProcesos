0f40a3a1eadce9670e140cf2863fd4221146b9d3
Marco Flores <59928598+bucksedey@users.noreply.github.com>
2024-12-15 03:14:12 -0600

    Add files via upload

---
+++ programa_1_sistemav.c
@@ -0,0 +1,68 @@
+//Flores Anzurez Marco Antonio 
+//Actividad 4 4CV1 Programa 1 Sistema V
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#define SHMKEY 75
+#define TRUE 1
+
+int region_critica();
+int region_no_critica(int pid);
+int esperando_turno(int pid);
+
+/// ESTE PROGRAMA SE CORRE PRIMERO para sistema V. programa_1_sistemav.c
+
+int main()
+{   // Declaracion de variables
+    int shmid, pid;
+    char *addr, *turno;
+    pid = getpid(); // Obtenemos el Pid del proceso
+    shmid = shmget(SHMKEY, 10, 0777 | IPC_CREAT);/* Crear la region de memoria y obtener la direcciòn*/
+    addr = shmat (shmid,0,0);/*ENLAZAR REGIÒN AL ESPACIO DE DIRECCIONAMIENTO DEL PROCESO*/
+    turno = (char *)addr; /*Reservar addr*/
+    
+    /*CODIG0 DE SINCRONIZACION*/
+    printf("\n [I] Variables Declaradas");
+    sleep (5); /*Para dar tiempo a que se ejecute el segundo proceso*/
+    (*turno) = '0'; // Declara el turno inicial
+    printf("\n [I] Iniciando proceso de turnos con turno inicial: %c",(*turno)); // Muestra el turno
+    while(TRUE){
+        while((*turno) != '0')esperando_turno(pid); // Espera a su turno
+        region_critica(pid); // manda a llamar a region ciritca
+        sleep(1); 
+        (*turno) = '1'; // Cambia el turno
+        region_no_critica(pid); // manda a llamar a region no critica
+    }
+    
+    /*Separar la regiòn del espacio de direccionamiento del proceso */
+    shmdt(addr);
+    /*Eliminar la region de memoria compartida*/
+    shmctl(shmid, IPC_RMID, 0);
+    return 0;
+} /*fin de la funciòn main*/
+
+
+int esperando_turno(int pid){
+    printf("\n [I] Proceso %d y esperando turno :p",pid);
+   
+    sleep(2);
+    return 0;
+}
+
+int region_critica(){
+    printf("\n\n [O] Cambiando Variables");
+     for(int i = 0; i < 5; i++){
+        printf("\n [O] Turno %d",i+1);
+        sleep(1);
+    }
+    return 0;
+}
+
+int region_no_critica(int pid){
+    printf("\n [O] Proceso %d y turno tomado :))))))))))))))))))))))))\n",pid);
+    return 0;
+}
